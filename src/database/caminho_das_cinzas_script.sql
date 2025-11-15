18e5098e3766e17489d9944c98dcfed56c04dc5f
MatheusPereira-bash <matheus.psantos@sptech.school>
2025-11-15 14:20:35 -0300

    Atualizando scripts do banco de dados

--- src/database/caminho_das_cinzas_script.sql
+++ src/database/caminho_das_cinzas_script.sql
@@ -40,57 +40,57 @@ CREATE TABLE tentativa_quiz (
 INSERT INTO quiz (titulo, descricao) VALUES ('Souls Quiz', 'Tente advinhar o local do mapa ou boss pela imagem');
 
 -- INSERINDO QUESTÕES SOULS QUIZ BLOODBORNE
-INSERT INTO questao (enunciado, imagem, alternativa_correta, id_quiz) VALUES
-	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/gehrman.jpg', 'A', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/kos.png', 'B', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/lady_maria.png', 'C', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/ludwig.png', 'C', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/micolash.jpg', 'A', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/rom.png', 'B', 1),
-	('Advinhe o mapa', 'assets/img/soulsQuiz/bloodborne/sonho_do_cacador.png', 'B', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/vigario_amelia.png', 'A', 1);
+INSERT INTO questao (enunciado, imagem, opcao1, opcao2, opcao3, alternativa_correta, id_quiz) VALUES
+	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/gehrman.jpg', 'Gehrman', 'Micolash', 'Orfão de Kos', 'A', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/kos.png', 'Ludwig', 'Orfão de Kos', 'Gehrman', 'B', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/lady_maria.png', 'Sister Friede', 'Rom', 'Lady Maria', 'C', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/ludwig.png', 'Midir', 'Padre Gascoigne', 'Ludwig', 'C', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/micolash.jpg', 'Micolash', 'Rom', 'Vigário Amélia', 'A', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/rom.png', 'Sif', 'Rom', 'Orfão de Kos', 'B', 1),
+	('Advinhe o mapa', 'assets/img/soulsQuiz/bloodborne/sonho_do_cacador.png', 'Ash Lake', 'Sonho do Caçador', 'Antiga Yharnam', 'B', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/bloodborne/vigario_amelia.png', 'Vigário Amélia', 'Artorias', 'Sulyvahn', 'A', 1);
     
 -- INSERINDO QUESTÕES SOULS QUIZ DARK SOULS 1
-INSERT INTO questao (enunciado, imagem, alternativa_correta, id_quiz) VALUES
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls1/artorias.jpg', 'A', 1),
-	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls1/ash_lake.jpg', 'B', 1),
-	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls1/firelink.png', 'C', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls1/gwyn.jpg', 'B', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls1/iron_golem.jpg', 'C', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls1/ornstein_smough.jpg', 'A', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls1/sif.jpg', 'A', 1);
+INSERT INTO questao (enunciado, imagem, opcao1, opcao2, opcao3, alternativa_correta, id_quiz) VALUES
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls1/artorias.jpg', 'Artorias', 'Sir Alonne', 'Gwyn', 'A', 1),
+	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls1/ash_lake.jpg', 'Majula', 'Ash Lake', 'Firelink', 'B', 1),
+	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls1/firelink.png', 'Anor Londo', 'New Londo Ruins', 'Firelink', 'C', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls1/gwyn.jpg', 'Soul of Cinder', 'Gwyn', 'Manus', 'B', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls1/iron_golem.jpg', 'Antigo Rei de Ferro', 'Sir Alonne', 'Iron Golem', 'C', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls1/ornstein_smough.jpg', 'Ornstein & Smough', 'Lorian & Lothric', 'Sif & Artorias', 'A', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls1/sif.jpg', 'Sif', 'Artorias', 'Manus', 'A', 1);
     
 -- INSERINDO QUESTÕES SOULS QUIZ DARK SOULS 2
-INSERT INTO questao (enunciado, imagem, alternativa_correta, id_quiz) VALUES
-	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls2/amana.jpg', 'A', 1),
-	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls2/majula.jpg', 'B', 1),
-	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls2/santuario_dragoes.jpg', 'B', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls2/antigo_rei_ferro.jpg', 'C', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls2/cavaleiro_espelho.jpg', 'A', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls2/sinh.jpg', 'C', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls2/velstadt.jpg', 'B', 1);
+INSERT INTO questao (enunciado, imagem, opcao1, opcao2, opcao3, alternativa_correta, id_quiz) VALUES
+	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls2/amana.jpg', 'Santuário de Amana', 'Majula', 'Floresta dos Gigantes Caídos', 'A', 1),
+	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls2/majula.jpg', 'Torre das Chamas de Heide', 'Majula', 'Bastilha Perdida', 'B', 1),
+	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls2/santuario_dragoes.jpg', 'Ravina Negra', 'Santuário dos Dragões', 'Tseldora' ,'B', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls2/antigo_rei_ferro.jpg', 'Apodrecido', 'Perseguidor', 'Antigo Rei de Ferro', 'C', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls2/cavaleiro_espelho.jpg', 'Cavaleiro do Espelho', 'Demônio da Fornalha', 'Rei de Marfim Queimado', 'A', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls2/sinh.jpg', 'Aava', 'Sir Alonne', 'Sinh', 'C', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls2/velstadt.jpg', 'Cavaleiro do Espelho', 'Velstadt', 'Nashandra', 'B', 1);
     
 -- INSERINDO QUESTÕES SOULS QUIZ DARK SOULS 3
-INSERT INTO questao (enunciado, imagem, alternativa_correta, id_quiz) VALUES
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/aldrich.jpg', 'A', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/friede.png', 'C', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/gael.jpg', 'B', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/lorian_lothric.jpg', 'B', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/midir.jpg', 'A', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/sulyvahn.jpg', 'C', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/yhorm.jpg', 'A', 1),
-	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls3/irithyll.jpg', 'B', 1);
+INSERT INTO questao (enunciado, imagem, opcao1, opcao2, opcao3, alternativa_correta, id_quiz) VALUES
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/aldrich.jpg', 'Aldrich', 'Sulyvahn', 'Vordt', 'A', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/friede.png', 'Yhorm', 'Aldrich', 'Sister Friede', 'C', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/gael.jpg', 'Campeão Gundyr', 'Gael', 'Rei Sem Nome', 'B', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/lorian_lothric.jpg', 'Sif & Artorias', 'Lorian & Lothric', 'Ornstein & Smough', 'B', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/midir.jpg', 'Midir', 'Aldrich', 'Oceiros', 'A', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/sulyvahn.jpg', 'Oceiros', 'Campeão Gundyr', 'Sulyvahn', 'C', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/darksouls3/yhorm.jpg', 'Yhorm', 'Gundyr', 'Vigilantes do Abismo', 'A', 1),
+	('Advinhe o mapa', 'assets/img/soulsQuiz/darksouls3/irithyll.jpg',  'Catedral das profundezas', 'Irithyll', 'Castelo de Lothric', 'B', 1);
     
 -- INSERINDO QUESTÕES SOULS QUIZ ELDEN RING
-INSERT INTO questao (enunciado, imagem, alternativa_correta, id_quiz) VALUES
-	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/godfrey.jpg', 'A', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/godrick.jpg', 'B', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/malenia.jpg', 'C', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/maliketh.jpg', 'C', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/radagon.jpg', 'A', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/rennala.jpg', 'B', 1),
-	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/rykard.jpg', 'C', 1),
-	('Advinhe o mapa', 'assets/img/soulsQuiz/eldenring/leyndell.jpg', 'C', 1);
+INSERT INTO questao (enunciado, imagem, opcao1, opcao2, opcao3, alternativa_correta, id_quiz) VALUES
+	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/godfrey.jpg', 'Godfrey', 'Radahn', 'Margit', 'A', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/godrick.jpg', 'Rykard', 'Godrick', 'Morgott', 'B', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/malenia.jpg', 'Mohg', 'Radahn', 'Malenia', 'C', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/maliketh.jpg', 'Radagon', 'Godfrey', 'Maliketh', 'C', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/radagon.jpg', 'Radagon', 'Godrick', 'Gideon Ofnir', 'A', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/rennala.jpg', 'Mogh', 'Rennala', 'Maliketh', 'B', 1),
+	('Advinhe o boss', 'assets/img/soulsQuiz/eldenring/rykard.jpg', 'Radahn', 'Godfrey', 'Rykard', 'C', 1),
+	('Advinhe o mapa', 'assets/img/soulsQuiz/eldenring/leyndell.jpg', 'Limgrave', 'Caelid', 'Leyndell', 'C', 1);
     
 select * from questao;
 select * from quiz;
